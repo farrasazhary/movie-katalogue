104e1b402459972f0ef3c10c5ba2654237084cd4
farrasazhary <farrasazhary18@gmail.com>
2023-10-10 16:53:04 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'd69715dcf9e244904b1ec23232eac42e',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
